cf1b7b528a682ff927e8d3f34ddcfbbf4f9daacb
Unknown <mattius.cardona@udea.edu.co>
2018-03-26 21:44:03 -0500

    Matriz

--- Solitario2/src/solitario/pkgint/pkg2/FXMLTableroController.java
+++ Solitario2/src/solitario/pkgint/pkg2/FXMLTableroController.java
@@ -9,6 +9,7 @@ import static java.awt.Color.blue;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -26,6 +27,7 @@ import javafx.scene.control.TextArea;
  */
 public class FXMLTableroController implements Initializable {
     
+    private Button[][] btns = new Button [8][8];
     @FXML
     private Button btn13;
     @FXML
@@ -98,12 +100,31 @@ public class FXMLTableroController implements Initializable {
     private ComboBox selecModo;
     @FXML
     private Button Acción;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         selecModo.getItems().addAll("Clásico", "Personalizado");
+        btns[1][3] = btn13; btns[1][4] = btn14; btns[1][5] = btn15; btns[2][3] = btn23; btns[2][4] = btn24;
+        btns[2][5] = btn25; btns[3][1] = btn31; btns[3][2] = btn32; btns[3][3] = btn33; btns[3][4] = btn34; 
+        btns[3][5] = btn35; btns[3][6] = btn36; btns[3][7] = btn37; btns[4][1] = btn41; btns[4][2] = btn42; 
+        btns[4][3] = btn43; btns[4][4] = btn44; btns[4][5] = btn45; btns[4][6] = btn46; btns[4][7] = btn47; 
+        btns[5][1] = btn51; btns[5][2] = btn52; btns[5][3] = btn53; btns[5][4] = btn54; btns[5][5] = btn55; 
+        btns[5][6] = btn56; btns[5][7] = btn57; btns[6][3] = btn63; btns[6][4] = btn64; btns[6][5] = btn65; 
+        btns[7][3] = btn73; btns[7][4] = btn74; btns[7][5] = btn75;
+        
+        
+        
     }    
     public void click(){
-        btn15.setStyle("-fx-background-image: url('file:/C:/Users/Asus/Desktop/Kraken/Solitario2/src/solitario/pkgint/IMG/CFondo.png');");
+        for(int i=1;i<=7;i++){
+            for(int j=1;j<=7;j++){
+                if((i<3 && j<3) || (i<3 && j>5) || (i>5 && j<3) || (i>5 && j>5)){
+                    btns[i][j]=null;
+                }else{
+                    btns[i][j].setStyle("-fx-background-image: url('file:/C:/Users/Asus/Desktop/Kraken/Solitario2/src/solitario/pkgint/IMG/SFondo.png');");
+                }
+            }
+        }
     }
     
 }
